1a85d22ba1a9ec104678da1e6d8c9fe4e7456a7d
Mirisan Catalin <catalinmirisan1@gmail.com>
2025-08-07 18:55:35 +0300

    Player and cirle added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -11,17 +12,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000.0
-
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
